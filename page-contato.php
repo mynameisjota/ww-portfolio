f39e81a1ed71f0653a8c91d60963239e817b945d
mynameisjota <webdevjota@gmail.com>
2018-12-25 21:31:31 -0200

    Criando a pagina de contato, adicionando arquivo de enviar.php

--- page-contato.php
+++ page-contato.php
@@ -8,11 +8,31 @@
 	<section class="contato">
 
 		<div class="left">
-
+			<h3>Dados</h3>
+				<span>11 953524954</span>
+				<span>hello@wwcomunicacao.com.br</span>
+				<h3 class="sociais-contato">Redes Sociais</h3>
+				<p>Redes Sociais Icones</p>
 		</div>
 
 		<div class="right">
 
+			<form action="<?php echo get_template_directory_uri(); ?>/enviar.php" method="post" name="form" class="formphp contato_form grid-8">
+				<label for="nome">Nome</label>
+				<input id="nome" name="nome" type="text">
+				<label for="email">E-mail</label>
+				<input id="email" name="email" type="text">
+				<label for="telefone">Telefone</label>
+
+				<textarea name="mensagem" id="mensagem"></textarea>
+
+				<div class="submmit">
+				<button id="enviar" name="enviar" type="submit" class="btn btn-preto">Enviar</button>
+
+				<p>Ou pelo e-mail: <a href="hello@wwcomunicacao.com.br">hello@wwcomunicacao.com.br</a></p>
+				</div>
+
+			</form>
 		</div>
 
 	</section> <!-- Servicos -->	
