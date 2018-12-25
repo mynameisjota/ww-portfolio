b057072f915d6700465ff9a5056f98da5c211a5a
mynameisjota <webdevjota@gmail.com>
2018-12-25 13:33:54 -0200

    Criação da página de contato

---
+++ page-contato.php
@@ -0,0 +1,24 @@
+<main class="container">
+<?php get_header('dois') ?>
+	
+	<h1 class="main-title-entry">Entre <br>
+		Em Contato
+	</h1>
+
+	<section class="contato">
+
+		<div class="left">
+
+		</div>
+
+		<div class="right">
+
+		</div>
+
+	</section> <!-- Servicos -->	
+
+<?php get_footer('dois') ?>
+</main>
+
+
+    
